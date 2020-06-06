c677caeab429308b1f21ad0eb86c339f750ec8a8
writepro4 <writepro4@naver.com>
2020-06-06 14:37:24 +0900

    [update] 6. css 클래스 파일 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { Component } from "react";
-import styled, { injectGlobal, css } from "styled-components";
+import React, {Component} from "react";
+import styled, {injectGlobal, ThemeProvider} from "styled-components";
+import theme from "./theme";
 
 injectGlobal`
   body{
@@ -8,12 +9,6 @@ injectGlobal`
   }
 `;
 
-const awesomeCard = css`
-  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
-  background-color: white;
-  border-radius: 10px;
-  padding: 20px;
-`;
 
 const Container = styled.div`
   height: 100vh;
@@ -21,24 +16,35 @@ const Container = styled.div`
   background-color: pink;
 `;
 
-const Input = styled.input.attrs({
-    required: true
-})`
-  border: none;
-  ${awesomeCard};
+const Card = styled.div`
+  background-color: red;
 `;
 
+const Button = styled.button`
+  border-radius: 30px;
+  padding: 25px 15px;
+  background-color: ${props => props.theme.successColor};
+`;
 
 
 class App extends Component {
     render() {
         return (
-            <Container>
-                <Input placeholder="hello"/>
-            </Container>
+            <ThemeProvider theme={theme}>
+                <Container>
+                    <Form/>
+                </Container>
+            </ThemeProvider>
         );
     }
 }
+
+const Form = () => (
+    <Card>
+        <Button>Hello</Button>
+    </Card>
+);
+
 
 export default App;
 
